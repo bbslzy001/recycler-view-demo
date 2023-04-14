f86577c93383933bd1d2c747ccabf1b022c44c45
bbslzy <bbslzy001@henu.edu.cn>
2023-04-14 19:20:04 +0800

    init.

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.java
@@ -1,26 +1,21 @@
 package com.example.recyclerviewdemo;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.MotionEvent;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Objects;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
     private RecyclerView recyclerView;
     private MyAdapter adapter;
-    private HeaderInfo headerInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -40,8 +35,9 @@ public class MainActivity extends AppCompatActivity
 
         adapter = new MyAdapter(this, getFakeData());  // 将假数据绑定到Adapter上
         recyclerView.setAdapter(adapter);  // 将Adapter绑定到RecyclerView上
-        recyclerView.addItemDecoration(new MyItemDecoration(this, adapter));
-        recyclerView.addOnItemTouchListener(new MyItemTouchListener());
+        MyItemDecoration itemDecoration = new MyItemDecoration(this, adapter);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.addOnItemTouchListener(itemDecoration);
     }
 
     /**
@@ -72,55 +68,5 @@ public class MainActivity extends AppCompatActivity
             dataList.add(new Group(headerData, itemDataList));
         }
         return dataList;
-    }
-
-    private class MyItemTouchListener implements RecyclerView.OnItemTouchListener
-    {
-        @Override
-        public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e)
-        {
-            LinearLayoutManager layoutManager = (LinearLayoutManager) rv.getLayoutManager();
-            int firstVisiblePosition = Objects.requireNonNull(layoutManager).findFirstVisibleItemPosition();
-
-            int firstVisibleViewType = adapter.getItemViewType(firstVisiblePosition); // 获取第一个可见的item的类型
-            if (firstVisibleViewType == MyAdapter.VIEW_TYPE_HEADER)  // 如果是组的头部
-            {
-                if (headerInfo == null) headerInfo = new HeaderInfo(rv.getChildAt(firstVisiblePosition));
-            }
-
-            // 获取触摸事件的坐标
-            // 参数 0 表示获取第一个触摸点的坐标，以避免数组越界异常
-            // 注意：下面的代码仅适用于单点触摸事件
-            float x = e.getX(0);
-            float y = e.getY(0);
-            // 获取触摸事件的动作
-            int action = e.getAction();
-            // 如果是按下动作
-            if (action == MotionEvent.ACTION_DOWN)
-            {
-                // 判断是否点击了HeaderView区域
-                if (x >= headerInfo.getHeaderLeft() && x <= headerInfo.getHeaderRight() && y >= headerInfo.getHeaderTop() && y <= headerInfo.getHeaderBottom())
-                {
-                    // 处理点击事件
-                    List<Integer> offsetList = adapter.getGroupIndexList();  // 获取所有 view 对应的 group 下标
-                    Group group = adapter.getDataList().get(offsetList.get(firstVisiblePosition)); // 获取当前组对象
-                    adapter.onHeaderClick(group);
-                    // 拦截触摸事件，不再传递给子View
-                    return true;
-                }
-            }
-            // 否则，按照默认的逻辑处理触摸事件
-            return false;
-        }
-
-        @Override
-        public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e)
-        {
-        }
-
-        @Override
-        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept)
-        {
-        }
     }
 }
--- app/src/main/java/com/example/recyclerviewdemo/MyItemDecoration.java
+++ app/src/main/java/com/example/recyclerviewdemo/MyItemDecoration.java
@@ -3,6 +3,7 @@ package com.example.recyclerviewdemo;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -15,7 +16,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
 
-public class MyItemDecoration extends RecyclerView.ItemDecoration
+public class MyItemDecoration extends RecyclerView.ItemDecoration implements RecyclerView.OnItemTouchListener
 {
     private final MyAdapter adapter;
     private final Context context;
@@ -32,7 +33,7 @@ public class MyItemDecoration extends RecyclerView.ItemDecoration
     @Override
     public void onDrawOver(@NonNull Canvas c, @NonNull RecyclerView parent, @NonNull RecyclerView.State state)
     {
-        if (state.isMeasuring() || adapter.getItemCount() == 0)  // 如果 recyclerview正在进行布局前的准备工作 或 数据为空
+        if (state.isMeasuring() || adapter.getItemCount() == 0)  // 如果recyclerview正在进行布局前的准备工作或数据为空
         {
             return; // 直接返回，不做任何操作
         }
@@ -86,5 +87,55 @@ public class MyItemDecoration extends RecyclerView.ItemDecoration
         headerView.measure(widthSpec, heightSpec);
         headerView.layout(headerInfo.getHeaderLeft(), headerInfo.getHeaderTop(), headerInfo.getHeaderRight(), headerInfo.getHeaderBottom());
         headerView.draw(c);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e)
+    {
+        if(adapter.getItemCount() == 0) return false;  // 如果recyclerView数据为空
+
+        LinearLayoutManager layoutManager = (LinearLayoutManager) rv.getLayoutManager();
+        int firstVisiblePosition = Objects.requireNonNull(layoutManager).findFirstVisibleItemPosition();
+
+        int firstVisibleViewType = adapter.getItemViewType(firstVisiblePosition); // 获取第一个可见的item的类型
+        if (firstVisibleViewType == MyAdapter.VIEW_TYPE_HEADER)  // 如果是组的头部
+        {
+            if (headerInfo == null) headerInfo = new HeaderInfo(rv.getChildAt(firstVisiblePosition));
+        }
+
+        // 获取触摸事件的坐标
+        // 参数 0 表示获取第一个触摸点的坐标，以避免数组越界异常
+        // 注意：下面的代码仅适用于单点触摸事件
+        float x = e.getX(0);
+        float y = e.getY(0);
+        // 获取触摸事件的动作
+        int action = e.getAction();
+        // 如果是按下动作
+        if (action == MotionEvent.ACTION_DOWN)
+        {
+            // 判断是否点击了HeaderView区域
+            if (x >= headerInfo.getHeaderLeft() && x <= headerInfo.getHeaderRight() && y >= headerInfo.getHeaderTop() && y <= headerInfo.getHeaderBottom())
+            {
+                // 处理点击事件
+                List<Integer> offsetList = adapter.getGroupIndexList();  // 获取所有 view 对应的 group 下标
+                Group group = adapter.getDataList().get(offsetList.get(firstVisiblePosition)); // 获取当前组对象
+                adapter.onHeaderClick(group);
+                //Objects.requireNonNull(layoutManager).scrollToPositionWithOffset(firstVisiblePosition, 0);  // 点击悬浮列表头，自动将recyclerview滚动到该列表头所在位置
+                // 拦截触摸事件，不再传递给子View
+                return true;
+            }
+        }
+        // 否则，按照默认的逻辑处理触摸事件
+        return false;
+    }
+
+    @Override
+    public void onTouchEvent(@NonNull RecyclerView rv, @NonNull MotionEvent e)
+    {
+    }
+
+    @Override
+    public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept)
+    {
     }
 }
